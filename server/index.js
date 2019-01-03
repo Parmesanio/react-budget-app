37c1ffd03641dd78407fa246bed4b05ef9e18ac5
Parmesanio <sean_parmar@yahoo.com>
2019-01-02 17:05:26 -0700

    Initial server, proxy, and component structure setup

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  massive = require("massive"),
+  session = require("express-session"),
+  app = express();
+require("dotenv").config();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 3600 * 24 * 7
+    }
+  })
+);
+app.use(express.static(`__dirname/../build`));
+massive(process.env.CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+    console.log("DB Set");
+  })
+  .catch(err => console.log("Err in Massive"));
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Application running on port PORT`));
